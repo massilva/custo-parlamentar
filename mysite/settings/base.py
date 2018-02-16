fec57ca53cf79607124a67a7ce0b4de19b550017
edelygomes <edelygomes@gmail.com>
2018-02-16 15:06:40 -0300

    trying configuration

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -83,8 +83,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'alba/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/alba/static/'
 
-MEDIA_ROOT = 'media/'
+MEDIA_ROOT = '/media/'
 MEDIA_URL = ''
