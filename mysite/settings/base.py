d7d010227833c3af99013038453ce457edf41331
edelygomes <edelygomes@gmail.com>
2018-02-20 15:31:46 -0300

    testando mudança da documentação

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -3,6 +3,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -83,7 +85,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = '/media/'
-MEDIA_URL = ''
+#MEDIA_ROOT = '/media/'
+#MEDIA_URL = ''
+
+django_heroku.settings(locals())
--- mysite/settings/development.py
+++ mysite/settings/development.py
@@ -1,5 +1,6 @@
 from .base import *
 import os
+import django_heroku
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', ''),
