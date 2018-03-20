c10b7ce5fc0e85cf507e627c2149586f57afb2e7
edelygomes <edelygomes@gmail.com>
2018-03-19 23:13:00 -0300

    view com gasto atual funcionando

--- alba/views.py
+++ alba/views.py
@@ -22,16 +22,8 @@ def gasto_mais_recente(deputado_id):
     return(data)
 
 def retorna_gastos(id_deputado, ano, mes):
-    # # print(deputado_id)
-    # print(ano)
-    # print(mes)
-    # # print(type(ano))
-    # # print(type(mes))
-    # print(id_deputado)
     dados = GastoMensal.objects.filter(ano=str(ano), id_deputado=id_deputado)
-    # print(dados)
     gastos = {'1': {}, '2': {}, '3': {}, '4': {}, '5': {}, '6': {}, '7': {}, '8': {}, '9': {}, '10': {}, '11': {}, '12': {}}
-    # print(gastos)
 
     categorias = ['10', '11', '12', '13', '14', '15']
 
@@ -42,14 +34,14 @@ def retorna_gastos(id_deputado, ano, mes):
     for gasto in dados:
         gastos[str(gasto.mes)][str(gasto.id_categoria.id_categoria)] = str(gasto.valor)
 
-    print(gastos)
-    aluguel_imoveis = gastos[mes]['10']
-    material_expediente = gastos[mes]['11']
-    locacao_software = gastos[mes]['12']
-    consultoria = gastos[mes]['13']
-    divulgacao = gastos[mes]['14']
-    hospedagem_locomocao = gastos[mes]['15']
-    context = {'gastos': gastos, 'gasto_atual':[aluguel_imoveis, material_expediente, locacao_software, consultoria, divulgacao, hospedagem_locomocao]}
+    aluguel_imoveis = gastos[str(mes)]['10']
+    material_expediente = gastos[str(mes)]['11']
+    locacao_software = gastos[str(mes)]['12']
+    consultoria = gastos[str(mes)]['13']
+    divulgacao = gastos[str(mes)]['14']
+    hospedagem_locomocao = gastos[str(mes)]['15']
+
+    context = {'gastos': gastos, 'gasto_atual': {'aluguel_imoveis': aluguel_imoveis, 'material_expediente': material_expediente, "locacao_software": locacao_software, 'consultoria': consultoria, 'divulgacao': divulgacao, 'hospedagem_locomocao': hospedagem_locomocao}}
     
     return context
 
@@ -77,22 +69,15 @@ def IndexView(request, slug='', ano='', mes=''):
         deputado_atual = 'Alba'
 
     try:
-        if slug != 'favicon.ico' and Deputados.objects.get(slug=slug):        
-            # print('-------')
-            # print(ano)
-            # print(mes)
+        if slug != 'favicon.ico' and Deputados.objects.get(slug=slug):  
             data_mais_recente = gasto_mais_recente(id_do_deputado)
             if ano == '':
                 ano = data_mais_recente['ano']
             if mes == '':
                 mes = data_mais_recente['mes']    
-            # print('&&&&&&&')
-            # print(ano)
-            # print(mes)
             gastos = retorna_gastos(id_do_deputado, ano, mes)
-            # print('=======')
-            # print(gastos)
-            # print('+++++++')
+            print(gastos['gasto_atual'])
+            context['gasto_atual'] = gastos['gasto_atual']
             context['gastos'] = gastos['gastos']
             
     except Exception as e:
